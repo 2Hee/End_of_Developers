326b3b43054b55efcd222e52650dfab0c6595d82
bobjones09 <smchild7@naver.com>
2022-10-30 03:32:35 +0900

    EnemyHealthBar midway

---
+++ End_of_Developers_Game/Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+
+    public float Hitpoints;
+    public float MaxHitpoints = 5;
+    public HealthBarBehaviour Healthbar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Hitpoints = MaxHitpoints;
+        Healthbar.SetHealth(Hitpoints, MaxHitpoints);
+    }
+
+    public void TakeHit(float damage)
+    {
+        Hitpoints -= damage;
+        Healthbar.SetHealth(Hitpoints, MaxHitpoints);
+
+        if (Hitpoints <= 0)
+        {
+            Destroy(gameObject);
+            Debug.Log("Bunny Destroyed");
+        }
+    }
+}
