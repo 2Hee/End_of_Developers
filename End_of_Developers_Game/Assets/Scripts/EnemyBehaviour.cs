9491118c70faf0d3bcb3b5ed660186a97786e070
bobjones09 <smchild7@naver.com>
2022-11-06 21:24:02 +0900

    min max attack number added

--- End_of_Developers_Game/Assets/Scripts/EnemyBehaviour.cs
+++ End_of_Developers_Game/Assets/Scripts/EnemyBehaviour.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyBehaviour : MonoBehaviour
 {
@@ -8,12 +9,21 @@ public class EnemyBehaviour : MonoBehaviour
     public float Hitpoints;
     public float MaxHitpoints = 30;
     public HealthBarBehaviour Healthbar;
+    public Text minNum;
+    public Text maxNum;
 
     // Start is called before the first frame update
     void Start()
     {
         Hitpoints = MaxHitpoints;
         Healthbar.SetHealth(Hitpoints, MaxHitpoints);
+        int num1 = Random.Range(1, 81);
+        int num2 = Random.Range(1, 21);
+        num2 += num1;
+
+        minNum.text = num1.ToString();
+        maxNum.text = num2.ToString();
+
     }
 
     public void TakeHit(float damage)
